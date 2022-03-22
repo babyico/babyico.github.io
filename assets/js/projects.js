446d055b2f7de88de6bb52a7b1e290776291773c
Laith /iconical <laith.daaja1@icloud.com>
2022-03-22 04:14:50 -0700

    Add files via upload

---
+++ assets/js/projects.js
@@ -0,0 +1,15 @@
+const projects = [
+    // EXAMPLE TEST
+    // {
+    //     "name": "name",
+    //     "description": "description",
+    //     "url": "#"
+    // },
+
+    {
+        "name": "Tower Blocks Game",
+        "description": "Just a simple game for testing some matters. plus to kill time ykyk ;D",
+        "url": "https://ico.is-a.dev/TowerBlocks/"
+    },
+    
+]
